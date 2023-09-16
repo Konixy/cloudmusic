ff131ffeb44d69a3c9f35b9991a3dba76976ee54
Konixy <konixy.p@gmail.com>
2023-09-16 14:13:56 +0200

    Update

---
+++ renderer/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fr" className="dark">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://kit-pro.fontawesome.com/releases/v6.3.0/css/pro.min.css"
+        />
+      </Head>
+      <body className="mt-10 bg-background">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
